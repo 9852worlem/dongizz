45cb1028c8d34b163b276da1481bc821b242d935
chomin <minnaldo6602@gmail.com>
2025-03-03 22:19:21 +0900

    SpringBoot Project Start

---
+++ dongizz-project/src/main/java/com/dongizz/dongizzproject/DongizzProjectApplication.java
@@ -0,0 +1,13 @@
+package com.dongizz.dongizzproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongizzProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DongizzProjectApplication.class, args);
+	}
+
+}
---
+++ dongizz-project/src/test/java/com/dongizz/dongizzproject/DongizzProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongizz.dongizzproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongizzProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
